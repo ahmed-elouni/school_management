0e11dd1c72579b91eef809a106f924c1c182e773
ahmed-elouni <aelouni3@gmail.com>
2025-06-16 04:14:55 +0200

    add a new api to get admin email that will be used in setting profile [router]

--- backend/routers/adminRegisterRouter.js
+++ backend/routers/adminRegisterRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import  { adminSignIn }  from "../controllers/usersControllers.js";
 import { adminRegister } from "../controllers/adminRegisterController.js";
+import { GetAdminEmail } from "../controllers/adminRegisterController.js";
 
 const router = express.Router();
 
 
 router.post('/signin', adminSignIn);
 router.post('/admin', adminRegister);
+router.get('/adminMail', GetAdminEmail);
 
 export default router;
